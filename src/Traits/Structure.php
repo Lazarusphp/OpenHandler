53f09345b62eb940433c766bda82102544db9ae5
mbamber1986 <30671636+mbamber1986@users.noreply.github.com>
2025-12-07 23:49:52 +0000

    Added Traits

---
+++ src/Traits/Structure.php
@@ -0,0 +1,125 @@
+<?php
+namespace LazarusPhp\OpenHandler\Traits;
+
+use Exception;
+
+trait Structure
+{
+    
+    
+    protected $directory = "";
+    protected $prefix = "";
+
+    
+
+    /**
+     * @method hasExtention
+     * @param string $filename
+     * @param string $extension
+     * @return bool;
+     */
+    protected function hasExtension(string $filename, string $extension):bool
+    {
+        $extention = $this->getFileExtension($filename);
+        return ($extention === $extension) ? true : false;
+    }
+
+
+
+
+    // Helper methods 
+
+    /**
+     * @method hasDirectory
+     * @property string $path
+     * @return void
+     * Helper function to detect if a directory exists.
+     */
+    protected function hasDirectory(string $path)
+    {
+        return is_dir($path) ? true : false;
+    }
+
+    //    Detect if file exists return bool
+
+    /**
+     * @method hasFile
+     * Detect if is a file
+     * @property string $path;
+     * @return bool 
+     */
+    protected function hasFile(string $path)
+    {
+          return (string) (is_file($path)) ? true : false;
+        
+    }
+
+    /**
+     * Detect if file exists
+     * @property string $path;
+     * @return bool
+     */
+    protected function fileExists(string $path)
+    {
+           return (file_exists($path)) ? true : false;
+        
+    }
+
+    /**
+     * @method hasDirectory
+     * @property string $path
+     * @method $this->whitelist() does a check for whitelisted values set within handler.
+     * @return void
+     * Helper function to detect if a directory exists.
+     */
+    protected function filePath(string $directory)
+    {
+            $root = $this->directory;
+            $prefix = $this->prefix ?? "";
+            $directory = $directory;
+
+            return (string) $root . $prefix . $directory;
+        
+    }
+
+    /**
+     * @method validMode
+     * @property int $mode
+     * Detrermines if the correct mode for directory creation is valid
+     */
+    protected function validMode(int $mode)
+    {
+            $modes = [0600, 0644, 0664, 0700, 0755, 0777];
+            if (in_array($mode, $modes)) {
+                return true;
+            } else {
+                return false;
+            }
+    }
+
+
+
+    /**
+     *  Detect if the Structure contains parent directorys
+     *  @property string $path
+     *  @return bool
+     */
+    protected function withDots($path)
+    {
+           return ($path === "." || $path === "..") ? true : false;
+    }
+
+    protected function getExtension(string $filename)
+    {
+        $extension = pathinfo($filename)["extension"];
+        return $extension;
+    }
+
+    protected function whitelistExtention(string $extension,array $whitelist)
+    {
+        if(!in_array($extension,$whitelist))
+        {
+            throw new Exception("Invalid Whitelist");
+        }
+    }
+}
